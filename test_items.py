620795494d7908f3da335187fa318c9b11936b82
Alina <selivanovaalina9@gmail.com>
2025-04-27 19:18:25 +0500

    init

---
+++ test_items.py
@@ -0,0 +1,14 @@
+import pytest
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+def test_add_to_cart_button_present(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+    WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, ".btn-add-to-basket"))
+    )
+    assert browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket"), "Кнопка добавления в корзину не найдена"
+    time.sleep(3)
