be54c1c632b01cb1112e3a02418adac26d2f2357
Alina <selivanovaalina9@gmail.com>
2025-04-28 22:41:30 +0500

    add page objects

---
+++ test_main_page.py
@@ -0,0 +1,12 @@
+import pytest
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from .pages import MainPage
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
